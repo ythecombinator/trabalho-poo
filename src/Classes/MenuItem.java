e9a7b7ad4e65aad12f85d0ef679a97c43463096b
Matheus Albuquerque <matheus.brasil10@gmail.com>
2018-10-16 03:07:40 -0400

    feat(models): MenuItem

---
+++ src/Classes/MenuItem.java
@@ -0,0 +1,15 @@
+package Classes;
+
+public class MenuItem {
+	public int id;
+	public String name;
+	public float price;
+	
+	public MenuItem(int id, String name, float price) {
+		this.id = id;
+		this.name = name;
+		this.price = price;
+	}
+	
+	public MenuItem() {}
+}
